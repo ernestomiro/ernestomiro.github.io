91b5f78b492fef5e382122d1baee339b0bc8b99d
emiro <emiro@itopia.com>
2026-09-06 03:10:43 -0400

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
   bcpp: {
-    enabled: false,
-    apiBaseUrl: '',
+    enabled: true,
+    apiBaseUrl:
+      'https://portafolio-api-ckhvfqa0fsadaga9.mexicocentral-01.azurewebsites.net',
     protectedPaths: ['/api/contact-messages'],
   },
 } as const;
